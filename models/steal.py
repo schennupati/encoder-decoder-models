3a52c813b4c6bf05ed57f2576580bdb97f4ddb06
Venkatraman <vijay4313@gmail.com>
2020-03-27 11:15:23 -0400

    Optimize NMS loss calculation

--- models/steal.py
+++ models/steal.py
@@ -12,10 +12,72 @@ class StealNMSLoss(nn.Module):
         self.grad2d = filters.SpatialGradient()
         self.nms_loss = 0
         self.r = r
+        self.eps = torch.finfo(pred_labels.dtype).eps
+        self.vert_k = torch.zeros((2*r-1, 2*r-1)).unsqueeze_(0)
+        self.vert_k[:, :, r-1] = 1
+        self.horiz_k = torch.zeros((2*r-1, 2*r-1)).unsqueeze_(0)
+        self.vert_k[:, r-1, :] = 1
+        self.lead_diag_k = torch.eye((2*r-1, 2*r-1)).unsqueeze_(0)
+        self.cnt_diag_k = torch.tensor(np.fliplr(np.eye(2*r-1))).unsqueeze_(0)
     
     def get_grads(self, true_labels):
         """
         Calculate the direstion of edges.
+        Ref: https://github.com/nv-tlabs/STEAL/blob/master/utils/edges_nms.m
+        """
+        first_gradients = self.grad2d(true_labels)
+        second_grad_x = self.grad2d(first_gradients[:, :, 0, :, :].squeeze_())
+        second_grad_y = self.grad2d(first_gradients[:, :, 1, :, :].squeeze_())
+
+        grad_yy = second_grad_y[:, :, 1, :, :].squeeze_()
+        grad_xy = second_grad_y[:, :, 0, :, :].squeeze_()
+        grad_xx = second_grad_x[:, :, 0, :, :].squeeze_()
+
+
+        theta = torch.fmod(torch.atan((grad_yy * torch.sign(-grad_xy + self.eps)) / (grad_xx + self.eps)), np.pi)
+        return theta
+
+    def __call__(self, true_labels, pred_labels):
+        """
+        NMS loss calculation.
+        """
+        # Find edge directions
+        theta = self.get_grads(true_labels)
+        thresh_theta = torch.fmod((theta * (5.0 / np.pi)).round() + 5, 5)
+        
+        # Create all possible direction NMS
+        exp_preds = torch.exp(pred_labels)
+        horiz_filter = filters.filter2D(exp_preds, self.horiz_k)
+        vert_filter = filters.filter2D(exp_preds, self.vert_k)
+        lead_diag_filter = filters.filter2D(exp_preds, self.lead_diag_k)
+        cnt_diag_filter = filters.filter2D(exp_preds, self.cnt_diag_k)
+
+        # Generate masked NMS
+        horiz_nms = torch.mul((thresh_theta == 0).float(), 
+                              torch.div(exp_preds, horiz_filter+self.eps)).unsqueeze_(1)
+        vert_nms = torch.mul((thresh_theta == 2).float(),
+                             torch.div(exp_preds, vert_filter+self.eps)).unsqueeze_(1)
+        lead_diag_nms = torch.mul((thresh_theta == 3).float(),
+                                  torch.div(exp_preds, lead_diag_filter+self.eps)).unsqueeze_(1)
+        cnt_diag_nms = torch.mul((thresh_theta == 1).float(),
+                                 torch.div(exp_preds, cnt_diag_filter+self.eps)).unsqueeze_(1)
+
+        all_nms = torch.cat((horiz_nms, vert_nms, lead_diag_nms, cnt_diag_nms), 1)
+        nms_loss = torch.sum(all_nms)
+
+        return nms_loss
+
+
+class StealNMSLossOld(nn.Module):
+    def __init__(self, r=2):
+        super(StealNMSLossOld, self).__init__()
+        self.grad2d = filters.SpatialGradient()
+        self.nms_loss = 0
+        self.r = r
+
+    def get_grads(self, true_labels):
+        """
+        Calculate the direction of edges.
         Ref: https://github.com/nv-tlabs/STEAL/blob/master/utils/edges_nms.m
         """
         eps = self.eps
